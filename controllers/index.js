58c088bef1d3acc71c21d0d73f36be8d5177fcb1
Darien Cheng <cheng.darien@gmail.com>
2020-08-11 14:04:07 -0400

    Create login page and route

--- controllers/index.js
+++ controllers/index.js
@@ -6,3 +6,7 @@ exports.get_index = function(req, res, next) {
 exports.get_signup = function(req, res, next) {
     res.render('signup', { title: 'Sign up | Opdoot' });
 }
+
+exports.get_login = function(req, res, next) {
+    res.render('login', { title: 'Log In | Opdoot' });
+}
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,7 @@ router.get('/', index.get_index);
 /* GET signup page. */
 router.get('/signup', index.get_signup);
 
+/* GET login page. */
+router.get('/login', index.get_login);
+
 module.exports = router;
