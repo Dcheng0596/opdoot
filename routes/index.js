1db67d8b51bd542a34aa8658e81f2e53a7c06ab8
Darien Cheng <cheng.darien@gmail.com>
2020-08-18 17:29:23 -0400

    Create sign up validation routes

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,16 @@ var router = express.Router();
 let index = require('../controllers/index');
 let user = require('../controllers/user');
 
-/* GET home page. */
 router.get('/', index.get_index);
 
-/* GET signup page. */
 router.get('/signup', user.get_signup);
 router.post('/signup', user.post_signup);
 
-/* GET login page. */
+router.post('/signup_ajax/email', user.validate_email);
+router.post('/signup_ajax/username', user.validate_username);
+router.post('/signup_ajax/password', user.validate_password);
+
 router.get('/login', user.get_login);
+router.get('/logout', user.get_logout);
 
 module.exports = router;
