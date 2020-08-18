6a9d8efa074a702fb79da8af5169ced7dafc8c57
Darien Cheng <cheng.darien@gmail.com>
2020-08-18 17:30:17 -0400

    Create input field validators

---
+++ helper/ajaxValidate.js
@@ -0,0 +1,54 @@
+let models = require('../db/models');
+let validator = require('validator');
+
+
+exports.validateEmail = (errors, email) => {
+    return new Promise((resolve, reject) => {
+        email = validator.trim(email);
+        if (!validator.isEmail(email)) {
+            errors["email"] = "Email is invalid or already taken";
+            resolve(errors);
+        } else {
+            return models.User.findOne({
+                where: {
+                    email: email
+                }
+            }).then(user => {
+                if (user !== null) {
+                    errors["email"] = "Email is invalid or already taken";
+                } 
+                resolve(errors);
+            })
+        }
+    });
+}
+
+exports.validateUsername = (errors, username) => {
+    return new Promise((resolve, reject) => {
+        if (!validator.isAlphanumeric(username)) {
+            errors["username"] = "Username can only contain letters and numbers";
+            resolve(errors);
+        } else if (!validator.isLength(username, {min: 3, max: 20})) {
+            errors["password"] = "Username must be between 3 and 20 characters long";
+            resolve(errors);
+        } else {
+            return models.User.findOne({
+                where: {
+                    username: username
+                }
+            }).then(user => {
+                if (user !== null) {
+                    errors["username"] = "Username is not available";
+                } 
+                resolve(errors);
+            })
+        }
+    });
+}
+
+exports.validatePassword = (errors, password) => {
+    if (!validator.isAscii(password))
+        errors["password"] = "Password contains invalid characters";		
+	if (!validator.isLength(password, {min: 8, max: 72}))
+        errors["password"] = "Password must be between 8 and 72 characters long";;
+}
--- helper/validate.js
+++ helper/validate.js
@@ -1,5 +1,4 @@
 let models = require('../db/models');
-
 let validator = require('validator');
 
 
@@ -18,18 +17,18 @@ const validateCreateUserFields = (errors, req) => {
     
     if (!validator.isAlphanumeric(req.body.username))
 		errors["username"] = "Username can only contain letters and numbers";
-    if (!validator.isLength(req.body.username, {min: 4, max: 20}))
-        errors["username"] = "Username must be between 4 and 20 characters long";
+    if (!validator.isLength(req.body.username, {min: 3, max: 20}))
+        errors["username"] = "Username must be between 3 and 20 characters long";
     if (validator.isEmpty(req.body.username))
 		errors["username"] = "Enter an username";
 }
 
 exports.validateUser = (errors, req, next) => {
-	return new Promise(function(resolve, reject) {
+	return new Promise((resolve, reject) => {
 		validateCreateUserFields(errors, req);
 		return models.User.findOne({
 			where: {
-				email: req.body.email
+				email: validator.trim(req.body.email)
 			}
 		}).then(user => {
 			if (user !== null) {
@@ -44,8 +43,6 @@ exports.validateUser = (errors, req, next) => {
                     errors["username"] = "Username is not available";
                 }
                 resolve(errors);
-             }).catch(err => {
-                 next(err);
              })
 		})
 	})
