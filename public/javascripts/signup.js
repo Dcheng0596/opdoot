25c76b3cb8eef72a79ec5cdcbc3f5451e903340f
Darien Cheng <cheng.darien@gmail.com>
2020-08-18 21:21:44 -0400

    Enable signup button when all inputs are validated

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,16 +1,61 @@
-//Email input event
+// number of inputs that need to be validated to submit the form
+let num_success = 3;
+
+let submit = document.querySelector("button[disabled]")
+
 document.getElementById("email").addEventListener("input", function() {
-    isValid('/signup_ajax/email', this.value)
+    isValid('/signup_ajax/email', this.value).then(errors => {
+        let error = document.getElementById("email-error");
+        let input = document.getElementById("email");
+        if("email" in errors) {
+           error.innerHTML = errors.email;
+           input.classList.add("error");
+           submit.setAttribute("disabled", "disabled")
+        } else {
+            error.innerHTML = "";
+            input.classList.replace("error", "success");
+            if(document.getElementsByClassName("success").length == num_success) {
+                submit.removeAttribute("disabled")
+            }
+        }
+    });
 });
 
 document.getElementById("username").addEventListener("input", function() {
-    isValid('/signup_ajax/username', this.value)
+    isValid('/signup_ajax/username', this.value).then(errors =>{
+        let error = document.getElementById("username-error");
+        let input = document.getElementById("username");
+        if("username" in errors) {
+           error.innerHTML = errors.username;
+           input.classList.add("error");
+           submit.setAttribute("disabled", "disabled")
+        } else {
+            error.innerHTML = "";
+            input.classList.replace("error", "success");
+            if(document.getElementsByClassName("success").length == num_success) {
+                submit.removeAttribute("disabled")
+            }
+        }
+    });
 });
 
 document.getElementById("password").addEventListener("input", function() {
-    isValid('/signup_ajax/password', this.value)
+    isValid('/signup_ajax/password', this.value).then(errors => {
+        let error = document.getElementById("password-error");
+        let input = document.getElementById("password");
+        if("password" in errors) {
+           error.innerHTML = errors.password;
+           input.classList.add("error");
+           submit.setAttribute("disabled", "disabled")
+        } else {
+            error.innerHTML = "";
+            input.classList.replace("error", "success");
+            if(document.getElementsByClassName("success").length == num_success) {
+                submit.removeAttribute("disabled")
+            }
+        }
+    });
 });
-
 
 const isValid = async (route, input) => {
     try {
@@ -24,7 +69,7 @@ const isValid = async (route, input) => {
             })
         });
         const resJson = await response.json();
-        console.log(resJson);
+        return resJson;
     } catch (error) {
         console.log(error)
     }
